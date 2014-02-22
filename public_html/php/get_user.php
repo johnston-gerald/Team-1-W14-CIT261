d0e53a79e9facedc2136da565ff8aeeff389a51c
Joseph Patterson <joseph@halfw.com>
2014-02-22 14:54:18 -0700

    Removed codeigniter and added ajax call to add user to database

---
+++ public_html/php/get_user.php
@@ -0,0 +1,20 @@
+<?php
+    $dbhost = 'localhost';	
+    $dbuser = 'cit261';
+    $dbpass = 'vyTxY7N69rURZnxu';
+    $dbname = 'listapp';
+    $db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+    if ($db->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+        exit();
+    }
+    
+    $user = $db->query("SELECT * FROM `users` WHERE id = " . $_POST['id']);
+    
+    if($user->num_rows){
+        //get users lists
+    } else {
+        //Add the user
+        $db->query("INSERT INTO `users` (`id`, `user_name`, `email`)  VALUES (" . $_POST['id'] . ", '" . $_POST['user_name'] . "', '" . $_POST['email'] . "')");
+    }
