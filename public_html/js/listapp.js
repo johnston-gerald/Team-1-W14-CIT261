8343edc04262f72cfb02aeb6eb141c58c4985850
johnston-gerald <joh11028@byui.edu>
2014-03-06 20:26:28 -0800

    Merge DOM
    Conflicts:
    	public_html/include/lists.html

--- public_html/js/listapp.js
+++ public_html/js/listapp.js
@@ -30,6 +30,49 @@ var listapp = (function() {
             }
         },
         showLists: function (){
+            
+            //create lists array and sort alphabetically by default
+var lists = [
+    "Shopping",
+    "To Do",
+    "Homework",
+    "Reminders",
+    "Work"
+].sort();
+//lists.reverse() will sort in descending order
+
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+        var href = document.createElement('a');
+        var span = document.createElement('span');
+        
+        // Set the contents:
+        href.appendChild(document.createTextNode(array[i]));
+        
+        // Set the URL and class
+        href.setAttribute('href', '#');
+        href.setAttribute('class', 'list');
+        span.setAttribute('class', 'glyphicon glyphicon-chevron-right');
+        item.appendChild(href);
+        item.appendChild(span);
+        
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Return the list:
+    return list;
+}
+// Add the contents of lists[] to #lists:
+document.getElementById('content').appendChild(makeUL(lists));
+
+console.log(makeUL(lists));
+            
             for(i=0;i < localStorage.lists;i++){
 
             }
