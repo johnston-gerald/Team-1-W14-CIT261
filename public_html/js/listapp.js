e7368e5bf68f29b39bb1ed915492ff0befe1557b
Joseph Patterson <joseph@halfw.com>
2014-03-19 20:52:08 -0600

    Fix lists

--- public_html/js/listapp.js
+++ public_html/js/listapp.js
@@ -287,7 +287,6 @@ var listapp = (function() {
             return list;
         },
         ajaxRequest: function(method, url, data){
-            data = JSON.stringify(data);
             var xmlhttp = new XMLHttpRequest();
             xmlhttp.open(method,url,true);
             xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
